b762a55c50012fcf932a07b538547e4e5ae822a2
BlackWasp <kylian.cresci1@gmail.com>
2021-12-02 20:31:55 +0100

    v1.0

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ filters = {
     'filetype':['Searches for a particular filetype mentioned in the query.', 'filetype:"pdf"'],
     'link':['Searches for external links to pages.', 'link:"keyword"'],
     'numrange':['Used to locate specific numbers in your searches.', 'numrange:321-325'],
-    'before':['Used to search within a particular date range.', 'filetype:pdf & (before:2000-01-01)'],
-    'after':['Used to search within a particular date range.', 'filetype:pdf & (after:2001-01-01)'],
+    'before':['Used to search within a particular date range.', '(before:2021-01-01)'],
+    'after':['Used to search within a particular date range.', '(after:2021-01-01)'],
     'allinanchor':[ '(and also inanchor)This shows sites which have the keyterms in links pointing to them, in order of the most links.','inanchor:rat' ],
     'allinpostauthor':[ '(and also inpostauthor)Exclusive to blog search, this one picks out blog posts that are written by specific individuals.', 'allinpostauthor:"keyword"'],
     'related':['List web pages that are “similar” to a specified web page.', 'related:www.google.com'],
-    'cache':['Shows the version of the web page that Google has in its cache.', 'cache:www.google.com']
+    'cache':['Shows the version of the web page that Google has in its cache.', 'cache:www.google.com'],
+    'keyword':['Searches for the occurrences of keywords.', '"keyword"']
 }
 
 request ={
@@ -36,9 +37,43 @@ request ={
     'cache':[],
 }
 
+window.onload = function(){
+    var selecto = document.getElementById('filter-selecto');
+    Object.entries(filters).forEach(([key, value]) => {
+        var newOption = document.createElement("option");
+        newOption.innerHTML = key;
+        selecto.appendChild(newOption);
+    });
+};
+
 document.getElementById("research-btn").addEventListener("click", function() {
     // console.log();
-    var uri= encodeURIComponent(document.getElementById("research-text").value)
+    var uri= encodeURIComponent(document.getElementById("request-text").value)
     if (uri){window.open("https://www.google.com/search?q="+uri)}
-    
+});
+
+document.getElementById("filter-btn").addEventListener("click", function() {
+    var textFilter = document.getElementById("filter-text").value;
+    if (textFilter){
+        document.getElementById("request-text").value += " "+textFilter;
+    }
+});
+
+document.getElementById("filter-clear-btn").addEventListener("click", function() {
+    document.getElementById("filter-text").value= ""
+});
+
+document.getElementById("request-clear-btn").addEventListener("click", function() {
+    document.getElementById("request-text").value= ""
+});
+
+
+document.getElementById("filter-selecto").addEventListener("change", function() {
+    var chosenFilter = document.getElementById("filter-selecto").value;
+    if (chosenFilter != "choose a filter") {
+        var dork = filters[chosenFilter][1];
+        var filterText = document.getElementById("filter-text")
+        if (filterText.value != "") {dork = " "+dork}
+        filterText.value += dork;
+    }
 });
